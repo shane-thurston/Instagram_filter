c726f9ebc8f45548fa0e51703b8f4b44b531d368
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2023-09-05 10:06:14 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+def execute_python_file(file_path):
+   try:
+      os.system(f'python {file_path}')
+   except FileNotFoundError:
+      print(f"Error: The file '{file_path}' does not exist.")
+
+execute_python_file('main_swap.py')
